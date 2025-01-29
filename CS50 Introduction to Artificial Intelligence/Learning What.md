5b4253f4d81027c5a489c2b9de11f805fb60bbb4
SinceToday <3028790558@qq.com>
2025-01-29 20:37:24 +0800

    pagerank complete

---
+++ CS50 Introduction to Artificial Intelligence/Learning What.md
@@ -0,0 +1,22 @@
+## Recording Learning What
+
+
+
+### Project: pagerank
+
+$ Core Formula: $
+$$
+PR(p) = \frac{1-d}{N} + d \sum_{i \in link(p)} \frac{PR(i)}{NumLinks(i)}
+$$
+首先是那个 $iterate\_pagerank$ 函数，我刚开始想用拓扑排序的想法，但是实际上是不对的，拓扑排序适用于有向无环图，但是很显然网页的链接并不一定满足无环的条件，导致拓扑排序有可能压根没有起点
+
+后来问了DeepSeek，估计也是标准写法，就是先不管计算 $PR(p)$ 时依赖的 $PR(i)$ 是不是准确的，就一直按照公式计算，直到与前一次的差别 $TotalDiff$ 小于一个设定的阈值 $Threshold$ ，就认为已经收敛，将答案作为正确值
+
+这种做法我以前从来没见过，虽然现在不能完全证明合理性，但是想象一下倒也合理
+
+至于 $sample\_pagerank$ 函数，基于 $Markov Chain$ ，得出转移概率分布，然后不断模拟（利用Python的`cur_page = random.choices(list(corpus.keys()), weights = transition.values(), k = 1)[0]` 函数），利用 $Monte Carlo Method$ 的思想得出概率分布，当然，核心在于函数是否能做到接近真随机
+
+
+
+
+
--- CS50 Introduction to Artificial Intelligence/pagerank/pagerank/pagerank.py
+++ CS50 Introduction to Artificial Intelligence/pagerank/pagerank/pagerank.py
@@ -2,8 +2,10 @@ import os
 import random
 import re
 import sys
+# from queue import Queue
+# from collections import defaultdict
 
-DAMPING = 0.85
+DAMPING = 0.852
 SAMPLES = 10000
 
 
@@ -57,6 +59,21 @@ def transition_model(corpus, page, damping_factor):
     linked to by `page`. With probability `1 - damping_factor`, choose
     a link at random chosen from all pages in the corpus.
     """
+
+    likehood = {}
+    pagenums = len(corpus)
+    linknums = len(corpus[page])
+    if linknums <= 0:
+        for key in corpus.keys():
+            likehood[key] = 1 / pagenums
+        return likehood
+    else:
+        for key in corpus.keys():
+            likehood[key] = ((1 - damping_factor) * 1) / pagenums
+        for linkpages in corpus[page]:
+            likehood[linkpages] += damping_factor / linknums
+        return likehood
+
     raise NotImplementedError
 
 
@@ -69,6 +86,21 @@ def sample_pagerank(corpus, damping_factor, n):
     their estimated PageRank value (a value between 0 and 1). All
     PageRank values should sum to 1.
     """
+
+    pagerank = {page : 0 for page in corpus}
+    cur_page = random.choice(list(corpus.keys()))
+
+    for _ in range(n):
+        pagerank[cur_page] += 1
+        transition = transition_model(corpus, cur_page, damping_factor)
+        cur_page = random.choices(list(corpus.keys()), weights = transition.values(), k = 1)[0]
+
+    total_times = sum(pagerank.values())
+    for page in pagerank:
+        pagerank[page] /= total_times
+
+    return pagerank
+
     raise NotImplementedError
 
 
@@ -81,6 +113,68 @@ def iterate_pagerank(corpus, damping_factor):
     their estimated PageRank value (a value between 0 and 1). All
     PageRank values should sum to 1.
     """
+
+    """
+    # wrong idea! using topological sort can not solve this kind of problems
+    # topological sort is appropriate for DAG, namely directed acyclic graph
+    # this problem can not be seen as a DAG
+
+    pagenums = len(corpus)
+    pagerank = {}
+    InDegree = {}
+    linkpages = defaultdict(list)
+    ZeroInDegree = Queue()
+    for key, value in corpus.items():
+        InDegree[key] = len(corpus[key])
+        pagerank[key] = (1 * (1.0 - damping_factor)) / pagenums
+        if InDegree[key] == 0:
+            ZeroInDegree.put(key)
+            continue
+        for page in value:
+            linkpages[page].append(key)
+    
+    while not ZeroInDegree.empty():
+        page = ZeroInDegree.get()
+        nums = len(corpus[page])
+        for link in corpus[page]:
+            pagerank[page] += (damping_factor * pagerank[link]) / nums
+        for link in linkpages[page]:
+            InDegree[link] -= 1
+            if InDegree[link] <= 0:
+                ZeroInDegree.put(link)
+
+    return pagerank
+    """
+
+    num_pages = len(corpus)
+    # if it not linked with any page, all pages will add 1 / page_nums
+    pagerank = {page: 1 / num_pages for page in corpus}    # create a dict
+    threshold = 0.0001                                     # Convergence threshold
+    change = float('inf')
+
+    while change > threshold:
+        new_pagerank = {}
+        total_diff = 0
+
+        for page in corpus:
+            new_pagerank[page] = (1 - damping_factor) / num_pages
+        
+            for linking_page in corpus:
+                if page in corpus[linking_page]:
+                    new_pagerank[page] += damping_factor * (pagerank[linking_page]) / len(corpus[linking_page])
+
+            total_diff += abs(new_pagerank[page] - pagerank[page])
+
+        pagerank = new_pagerank
+        change = total_diff
+        # print(pagerank)
+    
+    total = sum(pagerank.values())
+    for page in pagerank:
+        pagerank[page] /= total
+    
+    return pagerank
+
     raise NotImplementedError
 
 
